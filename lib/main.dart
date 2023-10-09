25c2de1630a0ca49425d73157eedacc85253731a
moyarishka <moyarishka@gmail.com>
2023-10-09 16:46:47 +0100

    unit test

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
-import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/notes_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verifyEmail_view.dart';
-import 'dart:developer' as console show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
