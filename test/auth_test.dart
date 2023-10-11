b51766bd1e1ecd7b8e5123f928edc6accb61313c
moyarishka <moyarishka@gmail.com>
2023-10-11 15:13:59 +0100

    adding firestore

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -112,7 +112,7 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializeException();
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false, email: 'test');
+    const user = AuthUser(isEmailVerified: false, email: 'test', id: '1');
     _user = user;
     return Future.value(user);
   }
@@ -130,7 +130,7 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializeException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true, email: 'test');
+    const newUser = AuthUser(isEmailVerified: true, email: 'test', id: '1');
     _user = newUser;
   }
 }
